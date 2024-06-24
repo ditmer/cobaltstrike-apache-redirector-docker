e543b043b4b51e5e8ee9a62b17ba924adf918822
ditmer <rdditmer3@gmail.com>
2024-06-24 12:12:03 -0600

    add cookie option, fix some bugs, try to be cool

--- create_config.sh
+++ create_config.sh
@@ -9,6 +9,7 @@ GREEN='\033[0;92m'
 RED='\033[0;31m'
 YELLOW='\033[0;93m'
 NC='\033[0m'
+MULTICONT=""
 
 
 # Trap Functions
@@ -174,6 +175,39 @@ function getCerts() {
 	done
 }
 
+function getMultiCondition() {
+    #
+    # This function gets the or/and condition needed for multiple rewrite conditions
+    #
+    
+    echo "Ok, we need to know how each condition should be met - should it be [OR] or [AND]"
+    echo " - [OR] requires that only one of the conditions match to return true"
+    echo " - [AND] requires that all conditions match to return true"
+    echo "We will only use [OR] or [AND] for all conditions, there is no mix or matching here"
+    while true; do
+        echo "Tell me what you want, tell me what you really really want:"
+        echo -e $GREEN
+        echo " 1. [OR]"
+        echo " 2. [AND]" 
+        echo -e $YELLOW
+        read -p "PiCK a NuMbER: " THECHOICE
+        case $THECHOICE in
+            1)
+                MULTICONT="OR"
+                break;
+                ;;
+            2)
+                MULTICONT="AND"
+                break;
+                ;;
+            *)
+                echo "NOT AN OPTION, STOP IT"
+                ;;
+        esac
+    done
+
+}
+
 function redirectRule() {
     #
     # This function creates the redirect rule for any rewrite condition
@@ -190,14 +224,14 @@ function redirectRule() {
         1)
             # Add redirect rule with teamserver protocol, ip, and port. It's party time!
             echo "  # Redirect traffic to ${CSPROTO}://${CSTEAMSERVER}:${CSPORT}" >> conf/apache_redirect.conf
-            echo "  RewriteRule ^.*$ ${CSPROTO}://${CSTEAMSERVER}:${CSPORT}%{REQUEST_URI} [P,NE]" >> conf/apache_redirect.conf            
+            echo "  RewriteRule ^.*$ ${CSPROTO}://${CSTEAMSERVER}:${CSPORT}%{REQUEST_URI} [L,P,NE]" >> conf/apache_redirect.conf            
             ;;
         2)
             # Add redirect rule with for custom location
             echo "Please enter the location you want to redirect to, in the following format 'protocol://domain_or_ip:port'. <-- NO TRAIL FORWARD SLASH"
             read REDLOCAL
             echo "  # Redirect traffic to ${REDLOCAL}" >> conf/apache_redirect.conf
-            echo "  RewriteRule ^.*$ ${REDLOCAL}%{REQUEST_URI} [P,NE]" >> conf/apache_redirect.conf
+            echo "  RewriteRule ^.*$ ${REDLOCAL}%{REQUEST_URI} [L,P,NE]" >> conf/apache_redirect.conf
             ;;
     esac
     # Add a space to the config file to seperate condition and rewrite rule chunks, for easy reading only
@@ -276,13 +310,14 @@ function URI(){
     echo "      2. /include/images/image1.jpg"
     echo "      3. /admin/"
     echo ""
+    echo "  You can specify multiple paths seperated by a comma: /testadmin/,/hellothere/,/download/images.zip"
+    echo ""
     echo "  Here are some keywords to use as well"
     echo -e "      1. ${YELLOW}random_char_path${GREEN} - This tells the script you want to create a condition rule for"
     echo "           a randon character URI, with a set length determined later. i.e. A target host making a GET request to"
     echo "           a random four character URI configured in the Malleable C2 profile"
     echo ""
     echo "  Finally, please note that for each URI condition you create, there must be a redirect rule that follows."
-    echo "  If you have multiple paths you want to use for one redirect rule, please add them all here with each being seperated by a comma"
     echo -e $YELLOW
     #fi
     echo -e "Please specify the paths you want to use in this one condition rule, multiple paths can be sperated by a comma (no spaces): "
@@ -291,6 +326,7 @@ function URI(){
     # If the url path entered contains a comma, we assume the user wanted to enter two paths. So split it and loop, ok
     if [[ $CONPATHS == *","* ]]; then
         let a=0
+        let f=0
         IFS=',' read -ra APATH <<< "$CONPATHS"
         for i in "${APATH[@]}"; do
             # Because we are assuming multiple paths were entered, we need to seperate each uri with the or clause - "|"
@@ -307,11 +343,34 @@ function URI(){
                 if [ ${i:0:1} != "/" ]; then
                     i="/${i}"
                 fi
-                if [ ${i: -1} != "/" ]; then
+                #if this is a file, don't add /
+                SUB='.'
+                if [[ "$i" == *"$SUB"* ]]; then
+                    f=1
+                elif [ ${i: -1} != "/" ]; then
                     i="${i}/"
+                fi
+                if (( $f == 0 )); then 
+                    echo -e $YELLOW
+                    echo -e "Do you want the path, ${i} , to match exactly or match this path and any subpaths under it? "
+                    echo -e " 1. Exact Match"
+                    echo -e " 2. Sub Path Match"
+                    read PATHMATCH
+                    case $PATHMATCH in
+                        1)
+                            ;;
+                        2)
+                            i="${i}(.*)"
+                            ;;
+                        *)
+                            echo "Not an option, we will decide for you - exact match it is"
+                            echo " It wasn't that hard...."
+                            ;;
+                    esac
                 fi
                 CONURL="${i}"
                 a=1
+                f=0
             else
                 # Catch random keyword
                 if [[ $i == *"random_char_path"* ]]; then
@@ -322,10 +381,33 @@ function URI(){
                 if [ ${i:0:1} != "/" ]; then
                     i="/${i}"
                 fi
-                if [ ${i: -1} != "/" ]; then
+                #if this is a file, don't add /
+                SUB='.'
+                if [[ "$i" == *"$SUB"* ]]; then
+                    f=1
+                elif [ ${i: -1} != "/" ]; then
                     i="${i}/"
                 fi
+                if (( $f == 0 )); then 
+                    echo -e $YELLOW
+                    echo -e "Do you want the path, ${i} , to match exactly or match this path and any subpaths under it? "
+                    echo -e " 1. Exact Match"
+                    echo -e " 2. Sub Path Match"
+                    read PATHMATCH
+                    case $PATHMATCH in
+                        1)
+                            ;;
+                        2)
+                            i="${i}(.*)"
+                            ;;
+                        *)
+                            echo "Not an option, we will decide for you - exact match it is"
+                            echo " It wasn't that hard...."
+                            ;;
+                    esac
+                fi
                 CONURL+="|${i}"
+                f=0
             fi
         done
 
@@ -335,6 +417,7 @@ function URI(){
     
     # No commas were present, so treat it as one uri
     else
+        let f=0
         # Catch random keyword
         if [[ $CONPATHS == *"random_char_path"* ]]; then
             getRandUri $CONPATHS
@@ -345,9 +428,32 @@ function URI(){
             CONPATHS="/${CONPATHS}"
         fi
         # We need to append/prepend the uri with forward slashes, because standards or whatever
-        if [ ${CONPATHS: -1} != "/" ]; then
+
+        SUB='.'
+        if [[ "$CONPATHS" == *"$SUB"* ]]; then
+            f=1
+        elif [ ${CONPATHS: -1} != "/" ]; then
             CONPATHS="${CONPATHS}/"
         fi
+        if (( $f == 0 )); then 
+            echo -e $YELLOW
+            echo -e "Do you want the path, ${i} , to match exactly or match this path and any subpaths under it? "
+            echo -e " 1. Exact Match"
+            echo -e " 2. Sub Path Match"
+            read PATHMATCH
+            case $PATHMATCH in
+                1)
+                    ;;
+                2)
+                    CONPATHS="${CONPATHS}(.*)"
+                    ;;
+                *)
+                    echo "Not an option, we will decide for you - exact match it is"
+                    echo " It wasn't that hard...."
+                    ;;
+            esac
+                fi
+
         echo "  # Conditional rule for url: ${CONPATHS}" >> conf/apache_redirect.conf
         echo "  RewriteCond %{REQUEST_URI} ^${CONPATHS}?$ [NC]" >> conf/apache_redirect.conf
     fi
@@ -386,18 +492,6 @@ function USERAGENT() {
     echo -e "Please specify the User Agent you want to use in this one condition rule: "
     read CONUSERA
 
-    # Catch blank user agent keyword
-    if [[ $CONUSERA == *"blank_user_agent"* ]]; then
-        echo "  # Blank user agent conditional rule" >> conf/apache_redirect.conf
-        echo "  RewriteCond %{HTTP_USER_AGENT} ^$ [NC]" >> conf/apache_redirect.conf
-    else
-        # No blank keyword, so business as usual
-        echo "  # Conditional rule for UserAgent: ${CONUSERA}" >> conf/apache_redirect.conf
-        
-        # Before we add the user agent to the config file all spaces, periods, and parentheses need to be escaped 
-        CONUSERA=$(sed -e 's/\./\\&/g;s/\ /\\&/g;s/)/\\&/g;s/(/\\&/g' <<< $CONUSERA)
-        echo "  RewriteCond %{HTTP_USER_AGENT} ^${CONUSERA}?$ [NC]" >> conf/apache_redirect.conf
-    fi
 
     # To make life a little easier, we will loop over this question so a user can add as many useragent strings for this one redirect rule
     while true; do
@@ -405,20 +499,40 @@ function USERAGENT() {
         read -p "Do you wan't to add another User Agent string for this one redirect rule? (y/N)" yn
         case $yn in
             [Yy]* )
+                # We don't need to ask for the OR/AND condition for this one. Mulitple useragent headers assumes an OR condition.
+
+                # Catch blank user agent keyword
+                if [[ $CONUSERA == *"blank_user_agent"* ]]; then
+                    echo "  # Blank user agent conditional rule" >> conf/apache_redirect.conf
+                    echo "  RewriteCond %{HTTP_USER_AGENT} ^$ [NC,OR]" >> conf/apache_redirect.conf
+                else
+                    # No blank keyword, so business as usual
+                    echo "  # Conditional rule for UserAgent: ${CONUSERA}" >> conf/apache_redirect.conf
+                    
+                    # Before we add the user agent to the config file all spaces, periods, and parentheses need to be escaped 
+                    CONUSERA=$(sed -e 's/\./\\&/g;s/\ /\\&/g;s/)/\\&/g;s/(/\\&/g' <<< $CONUSERA)
+                    echo "  RewriteCond %{HTTP_USER_AGENT} ^${CONUSERA}?$ [NC,OR]" >> conf/apache_redirect.conf
+                fi
+                
+                echo -e $YELLOW
                 echo -e "Please specify the User Agent you want to use in this one condition rule: "
                 read CONUSERA
-                # Catch blank user agent keywork
+                ;;
+            [Nn]* )
+
+                # Catch blank user agent keyword
                 if [[ $CONUSERA == *"blank_user_agent"* ]]; then
                     echo "  # Blank user agent conditional rule" >> conf/apache_redirect.conf
                     echo "  RewriteCond %{HTTP_USER_AGENT} ^$ [NC]" >> conf/apache_redirect.conf
                 else
+                    # No blank keyword, so business as usual
                     echo "  # Conditional rule for UserAgent: ${CONUSERA}" >> conf/apache_redirect.conf
-                    # Need to escape spaces, periods, and parentheses
+                    
+                    # Before we add the user agent to the config file all spaces, periods, and parentheses need to be escaped 
                     CONUSERA=$(sed -e 's/\./\\&/g;s/\ /\\&/g;s/)/\\&/g;s/(/\\&/g' <<< $CONUSERA)
                     echo "  RewriteCond %{HTTP_USER_AGENT} ^${CONUSERA}?$ [NC]" >> conf/apache_redirect.conf
                 fi
-                ;;
-            [Nn]* )
+
                 echo -e "${GREEN}OK fine${YELLOW}"
                 break
                 ;;
@@ -431,15 +545,72 @@ function USERAGENT() {
     fi
 }
 
-function BOTHCOND() {
+function COOKIE() {
+    #
+    # This is the function to create a conditional rule for a cookie header
+    #
+    
+    # This is the "readme for this section"
+    #if (( $1 == 0 )); then
+    echo -e $GREEN
+    echo "  For a Cookie redirect rule, all we need is the cookie name."
+    echo "  Example: _mysession"
+    echo ""
+    echo "  The cookie value match is a wildcard, so anything can be present in a cookie for the rule to trigger."
+    echo "  In the future we will look at specifying the value type to match - think base64 encoded, all numbers, or values of a specific length"
+    echo ""
+    echo "  Finally, please note that for these Cookie conditions you create now, there must be a redirect rule that follows."
+    echo -e $YELLOW
+    #fi
+    echo -e "Please specify the cookie name you want to use in this one condition rule: "
+    read COOKNAME
+    
+    while true; do
+        echo ""
+        read -p "Do you wan't to add another cookie header for this one redirect rule? (y/N)" yn
+        case $yn in
+            [Yy]* )
+                if [ -z "$MULTICONT" ]; then 
+                    getMultiCondition
+                fi
+
+                # Add Last COOKIE to config
+                echo "  # [${MULTICONT}] Conditional rule for cookie: ${COOKNAME}" >> conf/apache_redirect.conf
+                if [ "${MULTICONT}" == "AND" ]; then
+                    echo "  RewriteCond %{HTTP_COOKIE} ${COOKNAME}=([^;]+) [NC]">> conf/apache_redirect.conf
+                else
+                    echo "  RewriteCond %{HTTP_COOKIE} ${COOKNAME}=([^;]+) [NC,${MULTICONT}]">> conf/apache_redirect.conf
+                fi
+                
+                echo -e $YELLOW
+                echo -e "Please specify the cookie name you want to use in this one condition rule: "
+                read COOKNAME
+                
+                ;;
+            [Nn]* )
+                # Add Last COOKIE to config
+                echo "  # Conditional rule for cookie: ${COOKNAME}" >> conf/apache_redirect.conf
+                echo "  RewriteCond %{HTTP_COOKIE} ${COOKNAME}=([^;]+) [NC]">> conf/apache_redirect.conf
+
+                echo -e "${GREEN}OK, fine. Jerk${YELLOW}"
+                break
+                ;;
+        esac
+    done
+    if (( $2 == 1 )); then
+        redirectRule 0
+    fi
+}
+
+#function BOTHCOND() {
     #
     # This function is for adding both a URI path and User Agent string conditions for one redirect rule
     # Calls each funcion and passes a zero to disable the redirectRule call within each function 
     #
-    URI $1 0
-    USERAGENT $1 0
-    redirectRule 0
-}
+#    URI $1 0
+#    USERAGENT $1 0
+#    redirectRule 0
+#}
 
 function ADDCUST() {
     #
@@ -537,11 +708,11 @@ case $yn in
         NODOMAIN=0
         ;;
     [Nn]* )
-        REDIRECTOR_ADDR=$(curl -s https://canihazip.com/s)
+        REDIRECTOR_ADDR=$(curl -s ifconfig.io/ip)
         NODOMAIN=1
         ;;
     * )
-        REDIRECTOR_ADDR=$(curl -s https://canihazip.com/s)
+        REDIRECTOR_ADDR=$(curl -s ifconfig.io/ip)
         NODOMAIN=1
         ;;
 esac
@@ -610,7 +781,7 @@ while true; do
     echo -e ${GREEN}
     echo "  1. URI/URL Redirect Conditions"
     echo "  2. USER-AGENT Redirect Conditions"
-    echo "  3. Combination Of Both Conditions"
+    echo "  3. COOKIE Redirect Conditions"
     echo "  4. Enter Custom Redirect Condition(s)"
     echo "  5. Redirect All Traffic"
     echo "  6. I'm done"
@@ -624,7 +795,7 @@ while true; do
             USERAGENT $FIRSTTIME 1
             ;;
         3)
-            BOTHCOND $FIRSTTIME
+            COOKIE $FIRSTTIME 1
             ;;
         4)
             ADDCUST
@@ -656,13 +827,28 @@ echo "</VirtualHost>" >> conf/apache_redirect.conf
 echo -e $GREEN
 
 # Echo config to user
-echo "Here is the config file we just created: "
+echo "***************************************************"
 echo ""
+echo "Here is the config file we just created: "
+echo -e "${YELLOW}\n"
 cat conf/apache_redirect.conf
+echo ""
+echo -e "${GREEN}***************************************************"
+
+echo ""
+echo "***************************************************"
+echo ""
+echo -e "${YELLOW}Apache config located at: $(pwd)/conf/apache_redirect.conf"
+echo -e "${YEELOW}Certs are located at: $(pwd)/certs/"
+echo ""
+echo -e "${GREEN}***************************************************"
+echo ""
 
 # Echo docker run commands
 echo ""
-echo "To build and run the docker, please run the following commands: "
+echo "***************************************************"
+echo ""
+echo "To build and run the docker (If you want to), please run the following commands: "
 echo ""
 echo -e "  ---- To Build ----${YELLOW}"
 echo "  docker image build --tag apache-redirect ."
@@ -672,6 +858,10 @@ echo "  docker container run -p ${REDPORTNUMBER}:${REDPORTNUMBER} --name=apache-
 echo -e ${GREEN}
 echo -e "  ---- To Reload Apache after Cert/Config Change ----${YELLOW}"
 echo "  docker exec apache-redirect service apache2 reload"
+echo ""
+echo -e "${GREEN}***************************************************"
+echo ""
 echo -e $NC
+
 
 #END OF SCRIPT. SEE NOTHING FOLLOWS!
